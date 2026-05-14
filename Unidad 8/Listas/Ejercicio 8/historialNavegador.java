b7854a2bb79c0bd5107f40a643cab9bb6c858d5c
Pablo Manuel Fernández Velázquez <163730673+ProfitCoder@users.noreply.github.com>
2026-05-14 09:26:10 +0200

    Add files via upload

---
+++ Unidad 8/Listas/Ejercicio 8/historialNavegador.java
@@ -0,0 +1,43 @@
+//ProfitCoder      8:53        14/05/2026
+
+import java.util.LinkedList;
+
+public class historialNavegador{
+	public static void main(String [] args){
+		//Creation of colection that acts like a stack
+		LinkedList <String> historial = new LinkedList<>();
+		
+		//Add pages by order
+		historial.addFirst("https://www.google.com");
+		historial.addFirst("wikipedia.org/Java");
+		historial.addFirst("stackoverflow.com");
+		historial.addFirst("github.com");
+		//we made it like that to made it like a stack style
+		
+		//Ahora consultamos la página actual sin borrarla
+		System.out.println();
+		System.out.println("Página actual: " + historial.peekFirst());
+		
+		//Delete the last page, the oldest onefor restriction of size
+		historial.removeLast();
+		/*
+		Also, we can use:
+		historial.remove(historial.size()-1);
+		*/
+		
+		//Butom to go back
+		
+		/*
+		System.out.printf("Saliendo de %s, volviendo a %s\n",historial.removeFirst();,historial.peekFirst);
+		historial.add(historial.removeFirst(););
+		*/
+		
+		//Also we can use:
+		System.out.printf("Saliendo de %s, volviendo a %s\n",historial.removeFirst(),historial.peekFirst());
+		
+		//Print Historial
+		for(String s : historial){
+			System.out.println(s);
+		}
+	}
+}
